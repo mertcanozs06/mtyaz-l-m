14500f97ff317c6bbf7e91199acdb4b41396f710
Mertcan0637 <mertcan.ozsoy0637@gmail.com>
2025-06-29 00:00:52 +0300

    slick componenti düzenlendi

--- src/components/slider/Slick.jsx
+++ src/components/slider/Slick.jsx
@@ -1,44 +1,66 @@
-import React from 'react'
+import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BiFullscreen } from 'react-icons/bi';
 import image1 from '../../../src/assets/Images/images1.jpg';
 import image2 from '../../../src/assets/Images/images2.jpg';
 
 const Slick = () => {
-    var settings = {
+  const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
-  return (
-    <div className='w-full  overflow-hidden h-[200px] md:h-[400px] lg:h-[500px] mb-6'>
-    <Slider {...settings} className=''>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px]'> 
-        <img src={image1} className='w-full h-full object-cover object-center'/>
-      </div>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px] relative'> 
-        <h1 className='absolute left-110 top-110 text-4xl'>MT YAZILIMLA FİRMANIZI İLERİYE TAŞIYIN</h1>
-        <img src={image2} alt="imagesNull" className='w-full h-full object-cover object-center'/>
-      </div>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px]'>
-        <img src={image2} alt="imagesNull" className='w-full h-full object-cover object-center'/>
-      </div>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px]'>
-        <img src={image1} alt="imagesNull" className='w-full h-full object-cover object-center'/>
-      </div>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px]'>
-        <img src={image2} alt="imagesNull" className='w-full h-full object-cover object-center'/>
-      </div>
-      <div className='h-[200px] md:h-[400px] lg:h-[500px]'>
-        <img src={image1} alt="imagesNull" className='w-full h-full object-cover object-center'/>
-      </div>
-    </Slider>
-    </div> 
-    );
-}
 
-export default Slick
+  return (
+    <div className="w-screen overflow-hidden h-[250px] md:h-[400px] lg:h-[500px]">
+      <Slider {...settings}>
+        {/* SLIDE 1 */}
+        <div className="relative w-full h-[250px] md:h-[400px] lg:h-[500px]">
+          <img
+            src={image1}
+            alt="slide1"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+
+        {/* SLIDE 2 - Metin Üstünde */}
+        <div className="relative w-full h-[250px] md:h-[400px] lg:h-[500px]">
+          <img
+            src={image2}
+            alt="slide2"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+          <h1 className="absolute z-10 text-white text-xl md:text-4xl font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-[90%]">
+            MT YAZILIMLA FİRMANIZI İLERİYE TAŞIYIN
+          </h1>
+        </div>
+
+        {/* SLIDE 3 */}
+        <div className="relative w-full h-[250px] md:h-[400px] lg:h-[500px]">
+          <img
+            src={image2}
+            alt="slide3"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+
+        {/* SLIDE 4 */}
+        <div className="relative w-full h-[250px] md:h-[400px] lg:h-[500px]">
+          <img
+            src={image1}
+            alt="slide4"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Slick;
