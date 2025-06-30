8e1ec43523a0365cbb18a37fc0fd154ee0fe97b5
Mertcan0637 <mertcan.ozsoy0637@gmail.com>
2025-06-30 23:04:57 +0300

    Fiyatlar componentine slick dahil edildi

--- src/pages/fiyatlar/Fiyatlar.jsx
+++ src/pages/fiyatlar/Fiyatlar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import Slick from '../../components/slider/Slick'
 const Fiyatlar = () => {
   return (
     <div>
         <Header/>
-        
+        <Slick/>
     </div>
   )
 }
