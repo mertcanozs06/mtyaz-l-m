3aa077a157535ccb39162f61112a5a42b0b74a40
Mertcan0637 <mertcan.ozsoy0637@gmail.com>
2025-09-10 15:11:08 +0300

    Commit mesajınız

--- src/components/tanıtım-header/TanıtımHeaderContent.jsx
+++ src/components/tanıtım-header/TanıtımHeaderContent.jsx
@@ -5,7 +5,7 @@ const TanıtımHeaderContent = () => {
     <div className="bg-gray-800 text-white py-10 px-4">
       <div className="w-full max-w-5xl mx-auto text-center">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 leading-snug">
-          İşletmenizi kolayca <span className="text-sky-400">MT Yazılım</span> ile{' '}
+          İşletmenizi <span className="text-sky-400">MT Yazılım</span> ile{' '}
           <span className="text-sky-400">KOLAYCA</span> ve{' '}
           <span className="text-sky-400">TEK</span> yerden yönetin
         </h1>
